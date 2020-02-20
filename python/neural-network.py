b0b72360507e0c2fed6970d9789d387ac842b05a
Kennedy <matthew.kennedy1@stryker.com>
2020-02-20 09:38:04 -0500

    Added assertions for neural network class, as well as class definitions

--- python/neural-network.py
+++ python/neural-network.py
@@ -4,10 +4,66 @@ from node import node
 from weight import weight
 
 class neural_network:
-    def __init__(self):
-        return
+    """
+    Neural Network class
+
+    Instance variables
+    ------------------
+    nodes : (self.nodes)
+        np.array, matrix of all nodes
+    weights : (self.weights)
+        np.array, matrix of all weights
+    Learning rate : (self.alpha)
+        float, learning rate
+
+    Constructor
+    -----------
+    input nodes : in_size
+        int, number of input nodes
+    output nodes : out_size
+        int, number of output nodes
+    output function : out_func
+        str, name of the output function (default is softmax)
+    hidden layers : hl_count
+        int, number of hidden layers (can be None)
+    Hidden layer vars : **hl_s_af
+        strs, activation function for each hidden layer (ignored if h_l_count is None)
+
+    Global variables
+    ----------------
+    Function keywords : funcs
+        List(str), allowed function names
+    """
+    funcs = ['sigmoid', 'tanh', 'softmax']
+
+
     
-    def activation_function(self, input_values, name='Sigmoid'):
+    def __init__(self, in_size, out_size, out_func=None, hl_count=None, **hl_s_af):
+        #Verify sizes are numbers above 0
+        assert type(in_size) is int, "Size of input layer needs to be an int"
+        assert type(out_size) is int, "Size of output layer needs to be an int"
+        assert in_size >= 1, "Size of input layer must be a positive integer"
+        assert out_size >= 1, "Size of output layer must be a positive integer"
+        assert out_func is None or out_func in neural_network.funcs, "Function must be one of the following: " + ", ".join(neural_network.funcs)
+        
+        #Verify hidden layer elements
+        assert len(hl_s_af) == 2, "Hidden layer vars must have 2 items: Sizes and activation functions"
+        hl_vals = [value for value in hl_s_af.values()]
+
+        #Check each size
+        for size in hl_vals[0]:
+            try:
+                size = int(size)
+                assert size >= 1, "Hidden layer size must be int greater than 0"
+            except ValueError:
+                raise ValueError('Size must be of type int')
+        #Check each function
+        for a_f in hl_vals[1]:
+            assert a_f is None or a_f in neural_network.funcs, "Function must be one of the following: " + ", ".join(neural_network.funcs)
+
+        
+    
+    def activation_function(self, input_values, name='sigmoid'):
         """
         Runs value through the activation function for a neuron.
         Defaults to sigmoid function.
@@ -26,15 +82,16 @@ class neural_network:
             function as a list.
         """
 
-        if name == 'Sigmoid':
+        if name == 'sigmoid':
             return np.array([1 / (1 + math.exp(-x)) for x in input_values])
-        elif name == 'Tanh':
+        elif name == 'tanh':
             return np.tanh(input_values)
             
-
-node1 = node("a", "b")
-node2 = node("a", "b")
-weight1 = weight(node1, node2, 1.0)
+hidden_layer_vals = {"Sizes" : ("5", "10"), "Functions" : ("softmax", "tanh")}
+nn = neural_network(1, 1, **hidden_layer_vals)
+#node1 = node("a", "b")
+#node2 = node("a", "b")
+#weight1 = weight(node1, node2, 1.0)
 #arr = 200 * np.random.random_sample((10, 1)) - 100
 #nn = neural_network()
 #arr = nn.activation_function(arr, name='Tanh')
--- python/node.py
+++ python/node.py
@@ -12,22 +12,24 @@ class node:
         str, I, O, H#
     Activation function : (self.a_f)
         str, name of the activation function
+    Activation function : (self.s_f)
+        str, name of the squash function
     List of weights associated : (self.weights)
         np.array, array of associated weight objects
         Created after all nodes created for efficiency.
     """
-
-    id_count = 0
+    default_a_f = 'Sigmoid'
     
-    def __init__(self, layer, name, a_f=None):
+    def __init__(self, layer, name, a_f=None, s_f=None):
         assert layer is not None, "Layer must be a str, syntax: first letter of layer (I, H#, O)"
         assert name is not None, "Name must be a str, syntax: first letter of layer, # (I1, H12, O3)"
         assert type(layer) is str, "Layer must be a string"
         assert type(name) is str, "Name must be a string"
         assert type(a_f) is str or a_f is None, "Activation function (a_f) must be a str"
+        assert type(s_f) is str or s_f is None, "Activation function (a_f) must be a str"
 
-        
-        self.a_f = a_f if a_f is not None else 'Sigmoid'
+        self.a_f = a_f if a_f is not None else node.default_a_f
+        self.s_f = s_f
 
         
 #Testing purposes
