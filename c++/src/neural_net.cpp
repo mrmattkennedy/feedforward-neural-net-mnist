9950da9f30e4bccf0d5417e3bcb55f9758c0d005
Kennedy <matthew.kennedy1@stryker.com>
2020-03-16 16:08:02 -0400

    Finished feed forward function

--- c++/src/neural_net.cpp
+++ c++/src/neural_net.cpp
@@ -34,17 +34,20 @@ neural_net::~neural_net()
 
 void neural_net::create_arch()
 {
+	/*
 	std::vector<std::vector<double>> w1_init = init_weights(arch[0], arch[1]);
 	w1 = Eigen::MatrixXd(arch[0], arch[1]);
 	for (int i = 0; i < arch[0]; i++)
 		w1.row(i) = Eigen::VectorXd::Map(&w1_init[i][0], w1_init[i].size());
 	
 	//Doing 1 row is incredibly slow, need n rows by 1 column
+	
 	std::vector<std::vector<double>> b1_init = init_weights(arch[1], 1);
 	b1 = Eigen::MatrixXd(arch[1], 1);
 	for (int i = 0; i < arch[1]; i++)
 		b1.row(i) = Eigen::VectorXd::Map(&b1_init[i][0], b1_init[i].size());
-
+	
+	b1 = Eigen::VectorXd(arch[1]);
 	std::vector<std::vector<double>> w2_init = init_weights(arch[1], arch[2]);
 	w2 = Eigen::MatrixXd(arch[1], arch[2]);
 	for (int i = 0; i < arch[1]; i++)
@@ -59,6 +62,12 @@ void neural_net::create_arch()
 	b1_init.clear();
 	w2_init.clear();
 	b2_init.clear();
+	*/
+
+	w1 = Eigen::MatrixXd::Random(arch[0], arch[1]);
+	b1 = Eigen::MatrixXd::Random(1, arch[1]);
+	w2 = Eigen::MatrixXd::Random(arch[1], arch[2]);
+	b2 = Eigen::MatrixXd::Random(1, arch[2]);
 }
 
 
@@ -85,26 +94,22 @@ std::vector<std::vector<double>> neural_net::init_weights(int from_size, int to_
 
 void neural_net::feed_forward()
 {
-	printf("Here\n");
-	Eigen::MatrixXd a1 = (inputs * w1) + b1.transpose();
-	std::cout << a1.rows() << ", " << a1.cols() << std::endl;
-	//std::cout << a1_b.rows() << ", " << a1_b.cols() << std::endl;
-	std::cout << b1.rows() << ", " << b1.cols() << std::endl;
-	//a1 += a1_b;
-	printf("Here\n");
+	Eigen::MatrixXd a1 = inputs * w1;
+	for (int i = 0; i < a1.rows(); i++)
+		a1.row(i) += b1.row(0);
 	l1 = 1 / (1 + (a1.array() * -1).exp());
-	printf("Here\n");
-	Eigen::MatrixXd a2 = (l1 * w2);// + (b2 * l1);
-	printf("Here\n");
-	std::cout << a2.rows() << ", " << a2.cols() << std::endl;
-	
-	Eigen::MatrixXd sums = a2.rowwise().sum();
 
-	/*
-	//for (int i = 0; i < 
-	//l2 = a2_exp / sums;
-	std::cout << sums.rows() << ", " << sums.cols() << std::endl;
-	*/
+	Eigen::MatrixXd a2 = (l1 * w2);// + (b2 * l1);
+	for (int i = 0; i < a2.rows(); i++)
+		a2.row(i) += b2.row(0);
+	Eigen::MatrixXd a2_exp = a2.array().exp();
+	Eigen::MatrixXd a2_exp_sums = a2_exp.rowwise().sum();
+	
+	l2 = Eigen::MatrixXd(a2.rows(), a2.cols());
+	for (int i = 0; i < a2.rows(); i++)
+		l2.row(i) = a2_exp.row(i) / a2_exp_sums(i, 0);
+//	std::cout << l2.row(0) << std::endl << "Sum is " << l2.row(0).sum() << std::endl;
+
 }
 
 double neural_net::sigmoid(double z)
